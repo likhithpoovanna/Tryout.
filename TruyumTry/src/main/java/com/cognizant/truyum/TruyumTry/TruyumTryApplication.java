6463f97794d0359c61e5e08fda63cde8e028b8ae
likhithpoovanna <likhithpoovanna@gmail.com>
2021-07-07 21:28:26 +0530

    first commit

---
+++ TruyumTry/src/main/java/com/cognizant/truyum/TruyumTry/TruyumTryApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.truyum.TruyumTry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruyumTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruyumTryApplication.class, args);
+	}
+
+}
---
+++ TruyumTry/src/test/java/com/cognizant/truyum/TruyumTry/TruyumTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.truyum.TruyumTry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruyumTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
