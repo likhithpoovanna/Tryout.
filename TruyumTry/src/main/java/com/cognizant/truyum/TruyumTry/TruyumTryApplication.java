59aa55eb34c0d15e8c180d15052355db411aacd7
likhithpoovanna <likhithpoovanna@gmail.com>
2021-07-07 21:55:57 +0530

    comment

--- TruyumTry/src/main/java/com/cognizant/truyum/TruyumTry/TruyumTryApplication.java
+++ TruyumTry/src/main/java/com/cognizant/truyum/TruyumTry/TruyumTryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TruyumTryApplication {
-
+//main method.
 	public static void main(String[] args) {
 		SpringApplication.run(TruyumTryApplication.class, args);
 	}
